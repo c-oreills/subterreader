54ebb6710af4cf593ea4f6385c98900e3e673bab
Christy O'Reilly <christyor73@gmail.com>
2011-06-20 20:59:06 +0100

    Require login for subterreader

--- views.py
+++ views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def manage(request):
-  return HttpResponse("A list of docs that have yet to be read")
+  return HttpResponse('%s\'s list of docs that have yet to be read' % request.user.username)
 
+@login_required
 def settings(request):
-  return HttpResponse("User settings")
+  return HttpResponse('User settings')
 
+@login_required
 def read(request):
-  return HttpResponse("iFrames full of docs on reading list")
+  return HttpResponse('iFrames full of docs on reading list')
+

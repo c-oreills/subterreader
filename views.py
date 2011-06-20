53619afbee9ea563a0ab0e4ad5cfdcdfdcc503b3
Christy O'Reilly <christyor73@gmail.com>
2011-06-20 20:31:06 +0100

    Add skeleton subterreader views

--- views.py
+++ views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def manage(request):
+  return HttpResponse("A list of docs that have yet to be read")
+
+def settings(request):
+  return HttpResponse("User settings")
+
+def read(request):
+  return HttpResponse("iFrames full of docs on reading list")

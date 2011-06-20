95b20fdf5cc6953211c95ee1e3e899ac7de59003
Christy O'Reilly <christyor73@gmail.com>
2011-06-20 20:21:29 +0100

    Initial cut at document model

--- models.py
+++ models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Document(models.Model):
+  """
+  A webpage added to the reading list
+  """
+  url = models.CharField()
+  user = models.ForeignKeyField(User)
+  is_read = models.BooleanField()

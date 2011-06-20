7e189eb0bfb12521b6c2c777945609a2c673f342
Christy O'Reilly <christyor73@gmail.com>
2011-06-20 20:44:28 +0100

    Subterreader models: Correct fields and add user authentication

--- models.py
+++ models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Document(models.Model):
   """
   A webpage added to the reading list
   """
-  url = models.CharField()
-  user = models.ForeignKeyField(User)
+  url = models.CharField(max_length=255)
+  user = models.ForeignKey(User)
   is_read = models.BooleanField()

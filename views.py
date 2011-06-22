e83030095e3dd2c7c347e9d81fa966490d4a5cd7
Christy O'Reilly <christyor73@gmail.com>
2011-06-22 23:06:33 +0100

    Add static rendering and jquery include

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from subterreader.models import Document
 
 @login_required
@@ -15,5 +16,5 @@ def settings(request):
 @login_required
 def read(request):
   documents_list = Document.objects.filter(user=request.user).all()
-  return render_to_response('subterreader/read.html', {'documents_list': documents_list})
+  return render_to_response('subterreader/read.html', {'documents_list': documents_list}, RequestContext(request))
 

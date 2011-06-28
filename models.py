0aba1b7418a563316f311cf5e8c143ba988cf30e
Christy O'Reilly <christyor73@gmail.com>
2011-06-28 20:58:10 +0100

    Use builtin URLField type

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ class Document(models.Model):
   """
   A webpage added to the reading list
   """
-  url = models.CharField(max_length=255)
+  url = models.URLField(verify_exists=False, max_length=255)
   user = models.ForeignKey(User)
   is_read = models.BooleanField()
 

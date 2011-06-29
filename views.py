d9f08b861acbf2aec81b02e4c616de9d14f1349f
Christy O'Reilly <christyor73@gmail.com>
2011-06-29 22:22:15 +0100

    Remove all iframes code in favour of using AJAX and YQL

--- views.py
+++ views.py
@@ -17,7 +17,3 @@ def settings(request):
 def read(request):
   documents_list = Document.objects.filter(user=request.user).all()
   return render_to_response('subterreader/read.html', {'documents_list': documents_list}, RequestContext(request))
-
-@login_required
-def iframe(request, url):
-  return render_to_response('subterreader/iframe.html', {'url': url}, RequestContext(request))

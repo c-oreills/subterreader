e2cc130e36b33eb42e2d014f71cc85b5e1365ee4
Christy O'Reilly <christyor73@gmail.com>
2011-07-05 22:12:35 +0100

    Use new render shortcut to provide RequestContext automatically

--- views.py
+++ views.py
@@ -1,13 +1,13 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 from subterreader.models import Document
 
 @login_required
 def manage(request):
     documents_list = Document.objects.filter(user=request.user).all()
-    return render_to_response('subterreader/manage.html', {'documents_list': documents_list})
+    return render(request, 'subterreader/manage.html', {'documents_list': documents_list})
 
 @login_required
 def settings(request):
@@ -16,4 +16,4 @@ def settings(request):
 @login_required
 def read(request):
     documents_list = Document.objects.filter(user=request.user).all()
-    return render_to_response('subterreader/read.html', {'documents_list': documents_list}, RequestContext(request))
+    return render(request, 'subterreader/read.html', {'documents_list': documents_list})

3e1c1ab7fda908d2f7cafdcec591f35b91ea5745
Christy O'Reilly <christyor73@gmail.com>
2011-08-14 20:46:03 +0100

    Add add document form

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ModelForm
 
 # Create your models here.
 class Document(models.Model):
@@ -8,7 +9,7 @@ class Document(models.Model):
   """
   url = models.URLField(verify_exists=False, max_length=255)
   user = models.ForeignKey(User)
-  is_read = models.BooleanField()
+  is_read = models.BooleanField(default=False)
 
   def __unicode__(self):
     return self.url
@@ -16,3 +17,9 @@ class Document(models.Model):
   def clean(self):
     if '://' not in self.url[:10]:
       self.url = 'http://%s' % self.url
+
+
+class AddDocumentForm(ModelForm):
+    class Meta:
+        model = Document
+        fields = ('url',)

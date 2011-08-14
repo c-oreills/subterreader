37f49bb2a726188c25ddf331e41df8947442c3ed
Christy O'Reilly <christyor73@gmail.com>
2011-08-14 20:47:51 +0100

    Reindent models file

--- models.py
+++ models.py
@@ -4,19 +4,19 @@ from django.forms import ModelForm
 
 # Create your models here.
 class Document(models.Model):
-  """
-  A webpage added to the reading list
-  """
-  url = models.URLField(verify_exists=False, max_length=255)
-  user = models.ForeignKey(User)
-  is_read = models.BooleanField(default=False)
+    """
+    A webpage added to the reading list
+    """
+    url = models.URLField(verify_exists=False, max_length=255)
+    user = models.ForeignKey(User)
+    is_read = models.BooleanField(default=False)
 
-  def __unicode__(self):
-    return self.url
+    def __unicode__(self):
+        return self.url
 
-  def clean(self):
-    if '://' not in self.url[:10]:
-      self.url = 'http://%s' % self.url
+    def clean(self):
+        if '://' not in self.url[:10]:
+            self.url = 'http://%s' % self.url
 
 
 class AddDocumentForm(ModelForm):

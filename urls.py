b98bcec01d5a1f0f0d240d8d0df193b5fb33f9d7
Christy O'Reilly <christyor73@gmail.com>
2011-08-24 22:24:22 +0100

    Correct urls

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     (r'^$', 'subterreader.views.main'),
-    (r'^read$', 'subterreader.views.read'),
-    (r'^settings$', 'subterreader.views.settings'),
-    (r'^sample$', 'subterreader.views.sample'),
+    (r'^read/$', 'subterreader.views.read'),
+    (r'^settings/$', 'subterreader.views.settings'),
+    (r'^sample/$', 'subterreader.views.sample'),
 )
